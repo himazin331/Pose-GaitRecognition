26ca3331e27a21c7c804011010a5d7f2f58589ed
himazin331 <sion20022013@gmail.com>
2022-02-01 22:31:59 +0900

    Model for person identification

--- person_identification_model.py
+++ person_identification_model.py
@@ -3,19 +3,19 @@ import tensorflow.keras.layers as L
 
 # Person identification model (Simple CNN)
 class PersonIdentificationModel(tf.keras.Model):
-    def __init__(self, input_shape, ch=16):
+    def __init__(self, input_shape):
         super().__init__()
 
-        self.conv1 = L.Conv2D(ch, 4, activation='relu', input_shape=input_shape)
-        self.conv2 = L.Conv2D(ch*2, 4, activation='relu')
-        self.conv3 = L.Conv2D(ch*3, 4, activation='relu')
+        self.conv1 = L.Conv2D(16, 3, activation='relu', input_shape=input_shape)
+        self.conv2 = L.Conv2D(16, 3, activation='relu')
+        self.conv3 = L.Conv2D(32, 3, activation='relu')
 
         self.mp = L.MaxPool2D((2, 2), padding='same')
 
         self.fit = L.Flatten()
 
         self.dense = L.Dense(1024, activation='relu')
-        self.op_dense = L.Dense(2, activation='sigmoid')
+        self.op_dense = L.Dense(1, activation='softmax')
 
     def call(self, x):
         h1 = self.mp(self.conv1(x))
