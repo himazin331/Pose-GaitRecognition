b79062511915012e7332dda46f9374d5f0303e66
himazin331 <sion20022013@gmail.com>
2022-02-01 22:30:32 +0900

    run

--- run.py
+++ run.py
@@ -4,19 +4,39 @@ import argparse as arg
 from getPose import getPose
 from personIden import personIden
 
+from natsort import natsorted
+
+import cv2
+
+def read_file(dir_path):
+    file_list = []
+    for f in natsorted(os.listdir(dir_path)):
+        f = cv2.imread(os.path.join(dir_path, f))
+        f = cv2.cvtColor(f, cv2.COLOR_BGR2RGB)
+        file_list.append(f)
+    return file_list
+
 def runGaitRecog(frameA_path, frameB_path, outpath):
     gp = getPose()
+
     # Read video images
     frameA_list = gp.dataPrep(frameA_path)
     frameB_list = gp.dataPrep(frameB_path)
 
     # Person detection / Pose Estimation
-    gp.run(frameA_list, os.path.join(outpath, "A"))
-    gp.run(frameB_list, os.path.join(outpath, "B"))
+    jointsA_path = os.path.join(outpath, "A")
+    jointsB_path = os.path.join(outpath, "B")
+    gp.run(frameA_list, jointsA_path)
+    gp.run(frameB_list, jointsB_path)
+
+    # Read joint images
+    jointsA_list = read_file(jointsA_path)
+    jointsB_list = read_file(jointsB_path)
 
     # Person Identication
-    #personiden = personIden()
-    #personiden.run()
+    input_shape = jointsA_list[0].shape
+    personiden = personIden((None, input_shape[0], input_shape[1], input_shape[2]))
+    personiden.run(jointsA_list, jointsB_list)
 
     return False
 
@@ -33,12 +53,12 @@ def main():
 
     # Setting information
     print("=== Setting information ===")
-    print("# Video1 frame images path: {}".format(args.video_a))
-    print("# Video2 frame images path: {}".format(args.video_b))
+    print("# Video1 frame images path: {}".format(args.frame_a))
+    print("# Video2 frame images path: {}".format(args.frame_b))
     print("# Save location: {}".format(args.out_path))
     print("===========================\n")
 
-    err = runGaitRecog(args.video_a, args.video_b, args.out_path)
+    err = runGaitRecog(args.frame_a, args.frame_b, args.out_path)
     if err:
         print("error")
 
